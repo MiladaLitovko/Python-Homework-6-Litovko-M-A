0510d711589fe2e516bec0784c7959c459cf55c6
litovkomilada <litovkomiada@yandex.ru>
2022-09-22 14:33:23 +0300

    Task3

--- Task3.py
+++ Task3.py
@@ -28,5 +28,6 @@ import math
 list = [1.1, 1.2, 3.1, 5, 10.01]
 
 new_list = [list[i] % 1 for i in range(len(list))]
-result = str(lambda dif: max(new_list) - min(new_list))
-print(result[0:4])
+dif = str(max(new_list) - min(new_list))
+
+print(dif[0:4])
